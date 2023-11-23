389fd4efbe02e4afddfc5351f269e0af954e5ef6
pahansen <39408669+pahansen@users.noreply.github.com>
2023-11-23 18:40:52 +0100

    Refactor telemetry ingestion to use MongoDB

---
+++ f1_telemetry/data/mongodb_ingest.py
@@ -0,0 +1,51 @@
+"""Ingest F1 Telemetry to InfluxDB.
+"""
+from f1_telemetry.data.udp_stream import get_udp_messages
+from dotenv import load_dotenv
+import os
+from pymongo.mongo_client import MongoClient
+
+load_dotenv()
+
+MONGODB_CONNECTION_STRING = os.environ.get("MONGODB_CONNECTION_STRING")
+
+mongo_client = MongoClient(MONGODB_CONNECTION_STRING)
+
+def verify_mongodb_setup() -> None:
+    """Verify that db and collections exist in mongodb.
+    If they don't exist, create them.
+    """
+    db = mongo_client.f1
+    collections = [
+        "car_telemetry",
+        "car_status",
+        "car_setup",
+        "lap",
+        "motion",
+        "participants",
+        "session",
+        "car_damage",
+    ]
+    for collection in collections:
+        if collection not in db.list_collection_names():
+            db.create_collection(collection)
+
+def run_f1_telemetry_ingest() -> None:
+    """Run F1 telemetry ingestion.
+    """
+    verify_mongodb_setup()
+    for data in get_udp_messages():
+        data["m_header"]["m_sessionUID"] = str(data["m_header"]["m_sessionUID"])
+        packet_ids = {
+            0: "motion",
+            1: "session",
+            2: "lap",
+            4: "participants",
+            5: "car_setup",
+            6: "car_telemetry",
+            7: "car_status",
+            10: "car_damage"
+        }
+        message_type = packet_ids.get(data["m_header"]["m_packetId"], None)
+        if message_type is not None:
+            mongo_client.f1[message_type].insert_one(data)
